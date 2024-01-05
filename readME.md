53dd089d32a2c7fb8d47f0eed3f091c4892ae685
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2024-01-05 18:03:55 +0300

    starter

---
+++ readME.md
@@ -0,0 +1,11 @@
+## npx create-expo-app bookingapp
+    cd bookingapp
+    npx expo start
+## React Navigation 
+    npm install @react-navigation/native
+    npx expo install react-native-screens react-native-safe-area-context
+    npm install @react-navigation/native-stack
+
+## React native Bottom tab navigation 
+    npm install @react-navigation/bottom-tabs
+
---
+++ screens/HomeScreen.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+export default function HomeScreen() {
+    const navigation= useNavigation()
+    useLayoutEffect(()=>{
+        navigation.setOptions({
+            headerTitleAlign:'center',
+            headerShown:true,
+            title:"Booking.com",
+            headerTitleStyle:{
+             fonsSize:20,
+             FontWeight:"bold",
+             color:"white",
+            },
+            headerStyle:{
+                backgroundColor:"#003580",
+                height:110,
+               
+            }
+        })
+    },[])
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    
+})
