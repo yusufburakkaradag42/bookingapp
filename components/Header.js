f148759e48479765df620cd8b05afd7711a137c7
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2024-01-09 14:56:52 +0300

    Header.js

---
+++ components/Header.js
@@ -0,0 +1,84 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const Header = () => {
+    return (
+        <View style={{
+            backgroundColor: "#003580",
+            height: 65,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+        }}>
+
+            <Pressable style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                borderColor: 'white',
+                borderWidth: 2,
+                borderRadius: 25,
+                padding: 8,
+            }}>
+                <Ionicons name="bed-outline" size={24} color="white" />
+                <Text style={{
+                    marginLeft: 8,
+                    fontWeight: 'bold',
+                    color: 'white',
+                    fontSize: 15,
+                }}>Stays</Text>
+            </Pressable>
+
+            <Pressable style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+            
+            }}>
+               <Ionicons name="airplane-outline" size={24} color="white" />
+                <Text style={{
+                    marginLeft: 8,
+                    fontWeight: 'bold',
+                    color: 'white',
+                    fontSize: 15,
+                }}>Flights</Text>
+            </Pressable>
+
+
+            <Pressable style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              
+            }}>
+               <AntDesign name="car" size={24} color="white" />
+                <Text style={{
+                    marginLeft: 8,
+                    fontWeight: 'bold',
+                    color: 'white',
+                    fontSize: 15,
+                }}>Car Rental</Text>
+            </Pressable>
+
+
+            <Pressable style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+           
+            }}>
+               <FontAwesome5 name="taxi" size={24} color="white" />
+                <Text style={{
+                    marginLeft: 8,
+                    fontWeight: 'bold',
+                    color: 'white',
+                    fontSize: 15,
+                }}>Taxi</Text>
+            </Pressable>
+
+        </View>
+    )
+}
+
+export default Header
+
+const styles = StyleSheet.create({})
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import { Ionicons } from '@expo/vector-icons';
+import Header from '../components/Header';
+
 
 export default function HomeScreen() {
     const navigation= useNavigation()
@@ -10,20 +13,22 @@ export default function HomeScreen() {
             headerShown:true,
             title:"Booking.com",
             headerTitleStyle:{
-             fonsSize:20,
-             FontWeight:"bold",
+             fontSize: 20,
+             fontWeight:"bold",
              color:"white",
             },
             headerStyle:{
                 backgroundColor:"#003580",
-                height:110,
-               
-            }
+                height:110,  
+            },
+            headerRight:()=>(
+              <Ionicons name="notifications-outline" size={24} color="white" style={{marginRight:12}} />
+            )
         })
     },[])
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Header />
     </View>
   )
 }
